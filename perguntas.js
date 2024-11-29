828c7798e46213654757097e8cc933afa9726063
KillerJoy4532 <00001105311934sp@al.educacao.sp.gov.br>
2024-11-29 19:30:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Filmes',
+    'Qual o nome do lider dos autobots e decepticons no filme transformers?',
+    'optimus prime e megatron, são os principais lideres das duas forças roboticas bem e mal'
+)
+
+criaCartao(
+    'Series',
+    'Qual o nome dragão da montadora com o titulo de "rainha que nunca foi"?',
+    'Meleys a dragoa chamada de "rainha vermelha" era o dragão de daenys targaryan a mulher que carregava o titulo de a "rainha que nunca foi"'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual o filme da marvel causou a maior reviravolta dos cinemas, e que até hojem nao esquecem deles?',
+    'Vingadores guerra infinita e ultimato, foram os dois filmes com maiores reviravoltas e fãs da franquia marvel'
+)
+
+criaCartao(
+    'Series',
+    'Quem recebe o apelido de "garoto zumbi" em uma serie da netflix?',
+    'O personagem Will byers da serie "stranger things" ganhou ta apelido após certos acontecimentos (sem spoiler)'
+)
+
+criaCartao(
+    'Filmes',
+    'Quem e a figura famosa que diz a frase "qual o seu filme de terror favorito"?',
+    'Um dos mais famoso assasinos das telas de cinema e tv "ghostface" do iconico filme "panico"'
+)
+
+
+
+
+
+
